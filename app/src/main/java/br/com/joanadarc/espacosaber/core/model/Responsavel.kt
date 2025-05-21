fd76d57a0551773df6de20491bf80b1a67778161
Paulo Henrique Busato <paulo.busato2@gmail.com>
2025-05-21 16:20:27 -0300

    ajustado classe Responsavel para aceitar valores nulos

--- app/src/main/java/br/com/joanadarc/espacosaber/core/model/Responsavel.kt
+++ app/src/main/java/br/com/joanadarc/espacosaber/core/model/Responsavel.kt
@@ -1,13 +1,13 @@
 package br.com.joanadarc.espacosaber.core.model
 
 data class Responsavel(
-    val nome: String,
-    val cpf: String,
-    val dataNascimento: String,
-    val nacionalidade: String,
-    val telefone: String,
-    val email: String,
-    val rg: String,
-    val endereco: Endereco,
-    val cidadeNascimento: String,
+    val nome: String? = null,
+    val cpf: String? = null,
+    val dataNascimento: String? = null,
+    val nacionalidade: String? = null,
+    val telefone: String? = null,
+    val email: String? = null,
+    val rg: String? = null,
+    val endereco: Endereco? = null,
+    val cidadeNascimento: String? = null,
 )
