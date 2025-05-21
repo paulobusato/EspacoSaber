05077df06369ef49c060dac64b4dbf649e47bb9a
Paulo Henrique Busato <paulo.busato2@gmail.com>
2025-05-21 09:38:01 -0300

    adicionado hilt

---
+++ app/src/main/java/br/com/joanadarc/espacosaber/EsApplication.kt
@@ -0,0 +1,8 @@
+package br.com.joanadarc.espacosaber
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EsApplication : Application() {
+}
--- app/src/main/java/br/com/joanadarc/espacosaber/MainActivity.kt
+++ app/src/main/java/br/com/joanadarc/espacosaber/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.joanadarc.espacosaber.ui.theme.EspacoSaberTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
