c4218357847609df692a0fa94aa5702c9662d667
Paulo Henrique Busato <paulo.busato2@gmail.com>
2025-05-21 10:37:46 -0300

    adicionado kotlin serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -58,6 +59,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.navigation.compose)
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
