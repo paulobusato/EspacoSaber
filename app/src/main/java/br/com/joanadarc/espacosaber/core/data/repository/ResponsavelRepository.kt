53ee6ffdca3e8ba69bccb14bee657b1383bcfad2
Paulo Henrique Busato <paulo.busato2@gmail.com>
2025-05-22 15:49:10 -0300

    adicionado metodo para obter todos os responsaveis

--- app/src/main/java/br/com/joanadarc/espacosaber/core/data/repository/ResponsavelRepository.kt
+++ app/src/main/java/br/com/joanadarc/espacosaber/core/data/repository/ResponsavelRepository.kt
@@ -8,6 +8,7 @@ import javax.inject.Inject
 
 interface ResponsavelRepository {
     fun obterResponsavel(responsavelId: String): Flow<Responsavel>
+    fun obterResponsaveis(): Flow<List<Responsavel>>
 }
 
 class ResponsavelRepositoryImpl @Inject constructor() : ResponsavelRepository {
@@ -31,5 +32,48 @@ class ResponsavelRepositoryImpl @Inject constructor() : ResponsavelRepository {
             ),
             cidadeNascimento = "São Paulo",
         ),
+    )
+
+    override fun obterResponsaveis(): Flow<List<Responsavel>> = flowOf(
+        listOf(
+            Responsavel(
+                nome = "João Silva",
+                cpf = "987.654.321-00",
+                dataNascimento = "05/05/1980",
+                nacionalidade = "Brasileira",
+                telefone = "(11) 88888-8888",
+                email = "john.jay@example.com",
+                rg = "98.765.432-1",
+                endereco = Endereco(
+                    logradouro = "Rua Exemplo",
+                    numero = "456",
+                    complemento = "Casa 2",
+                    bairro = "Centro",
+                    cep = "54321-876",
+                    cidade = "São Paulo",
+                    estado = "SP",
+                    ),
+                cidadeNascimento = "São Paulo",
+            ),
+            Responsavel(
+                nome = "Maria Santos",
+                cpf = "123.456.789-00",
+                dataNascimento = "15/03/1950",
+                nacionalidade = "Brasileira",
+                telefone = "(11) 77777-7777",
+                email = "william.henry.harrison@example-pet-store.com",
+                rg = "12.345.678-9",
+                endereco = Endereco(
+                    logradouro = "Rua Exemplo",
+                    numero = "7",
+                    complemento = "Casa 1",
+                    bairro = "Centro",
+                    cep = "98765-432",
+                    cidade = "São Paulo",
+                    estado = "SP",
+                ),
+                cidadeNascimento = "São Paulo",
+            ),
+        )
     )
 }
